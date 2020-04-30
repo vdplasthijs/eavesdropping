4fbe98b013fee11b0283051ee8fa8d14a3dd989a
thijs-bokma <vdplasthijs@gmail.com>
2020-04-30 21:03:10 +0100

    Decoder double seq

--- BPTTRNN.py
+++ BPTTRNN.py
@@ -119,4 +119,25 @@ def compute_full_pred(xdata, model):
             _, full_pred[kk, tt, :] = model(xdata[kk, tt, :])  # compute prediction at this time 
     return full_pred
     
+def angle_vecs(v1, v2):
+    """Compute angle between two vectors with cosine similarity.
 
+    Parameters
+    ----------
+    v1 : np.array
+        vector 1.
+    v2 : np.array
+        vector 2.
+
+    Returns
+    -------
+    deg: float
+        angle in degrees .
+
+    """
+    assert v1.shape == v2.shape
+    v1, v2 = np.squeeze(v1), np.squeeze(v2)
+    tmp = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    rad = np.arccos(tmp)
+    deg = rad * 360 / (2 * np.pi)
+    return deg
