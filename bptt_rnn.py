7e83f774d34ff5eaf9a000fa32138b9fa538c8c3
thijs-bokma <vdplasthijs@gmail.com>
2020-05-06 08:16:39 +0100

    full training & decoder fun

--- bptt_rnn.py
+++ bptt_rnn.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 import pickle, datetime, time
 from tqdm import tqdm, trange
+import sklearn.svm
 
 def generate_synt_data(n_total=100, n_times=9, n_freq=8,
                        ratio_train=0.8, ratio_exp=0.5,
@@ -93,6 +94,7 @@ class RNN(nn.Module):
 
         self.train_loss_arr = []  # to be appended during training
         self.test_loss_arr = []
+        self.decoding_crosstemp_score = None
 
     def init_state(self):
         '''Initialise hidden state to random values N(0, 0.1)'''
@@ -155,7 +157,7 @@ def compute_full_pred(xdata, model):
     return full_pred
 
 def bptt_training(rnn, optimiser, dict_training_params,
-                  x_train, x_test, y_train, y_test):
+                  x_train, x_test, y_train, y_test, verbose=1):
     '''Training algorithm for backpropagation through time, given a RNN model, optimiser,
     dictionary with training parameters and train and test data. RNN is NOT reset,
     so continuation training is possible. Training can be aborted prematurely by Ctrl+C,
@@ -172,7 +174,8 @@ def bptt_training(rnn, optimiser, dict_training_params,
         rnn.info_dict['trained_epochs'] = 0
 
     ## Training procedure
-    init_str = f'Initialising training; start at epoch {rnn.info_dict["trained_epochs"]}'
+    if verbose >= 0:
+        init_str = f'Initialising training; start at epoch {rnn.info_dict["trained_epochs"]}'
     time.sleep(0.1)  # so printing doesn't interfere wtih tqdm bar
     try:
         with trange(dict_training_params['n_epochs']) as tr:  # repeating epochs
@@ -217,9 +220,87 @@ def bptt_training(rnn, optimiser, dict_training_params,
 
         ## Set to evaluate mode and cutoff for early termination
         rnn.eval()
-        print('Training finished. Results savedin RNN Class')
+        if verbose >= 0:
+            print('Training finished. Results savedin RNN Class')
         return rnn
     except KeyboardInterrupt: # end prematurely by Ctrl+C
         rnn.eval()
-        print(f'Training ended prematurely by user at epoch {epoch}.\nResults saved in RNN Class.')
+        if verbose >= 0:
+            print(f'Training ended prematurely by user at epoch {epoch}.\nResults saved in RNN Class.')
         return rnn
+
+def train_decoder(rnn_model, x_train, x_test, labels_train, labels_test,
+                  dict_training_params, save_inplace=True):
+    forw_mat = {'train': np.zeros((x_train.shape[0], x_train.shape[1], dict_training_params['n_nodes'])),
+                'test': np.zeros((x_test.shape[0], x_test.shape[1], dict_training_params['n_nodes']))}
+
+    rnn_model.eval()
+    with torch.no_grad():
+        ## Forward runs:
+        for kk in range(x_train.shape[0]):  # trial loop
+            rnn_model.init_state()
+            hidden_state =  rnn_model.state  # init state
+            for tau in range(x_train.shape[1]):  # time loop
+                hidden_state, output =  rnn_model.forward(inp=x_train[kk, tau, :],
+                                                   rnn_state=hidden_state)  # propagate
+                forw_mat['train'][kk, tau, :] = hidden_state.numpy()  # save hidden states
+
+        for kk in range(x_test.shape[0]):  # trial loop
+            rnn_model.init_state()
+            hidden_state =  rnn_model.state
+            for tau in range(x_test.shape[1]):  # time loop
+                hidden_state, output =  rnn_model.forward(inp=x_test[kk, tau, :],
+                                                   rnn_state=hidden_state)
+                forw_mat['test'][kk, tau, :] = hidden_state.numpy()
+
+        ## Train decoder
+        alpha_labels = {'train': np.array([int(x[0]) for x in labels_train]),
+                        'test': np.array([int(x[0]) for x in labels_test])}
+        score_mat = np.zeros((x_train.shape[1], x_train.shape[1]))  # T x T
+        decoder_dict = {}  # save decoder per time
+        for tau in range(x_train.shape[1]):  # train time loop
+            decoder_dict[tau] = sklearn.svm.LinearSVC(C=1e-2)  # define SVM
+            decoder_dict[tau].fit(X=forw_mat['train'][:, tau, :],
+                                  y=alpha_labels['train'])  # train SVM
+            for tt in range(x_train.shape[1]):  # test time loop
+                score_mat[tau, tt] = decoder_dict[tau].score(X=forw_mat['test'][:, tt, :],
+                                                             y=alpha_labels['test'])  # evaluate
+    if save_inplace:
+        rnn_model.decoding_crosstemp_score = score_mat
+    return score_mat, decoder_dict
+
+def init_train_save_rnn(t_dict, d_dict, n_simulations=1):
+    try:
+        for nn in range(n_simulations):
+            print(f'\n-----------\nsimulation {nn}/{n_simulations}')
+            ## Generate data:
+            tmp0, tmp1 = generate_synt_data(n_total=d_dict['n_total'],
+                                       n_times=d_dict['n_times'],
+                                       n_freq=d_dict['n_freq'],
+                                       ratio_train=d_dict['ratio_train'],
+                                       ratio_exp=d_dict['ratio_exp'],
+                                       noise_scale=d_dict['noise_scale'],
+                                       double_length=d_dict['doublesse'])
+            x_train, y_train, x_test, y_test = tmp0
+            labels_train, labels_test = tmp1
+
+            ## Initiate RNN model
+            rnn = RNN(n_stim=d_dict['n_freq'], n_nodes=t_dict['n_nodes'])  # Create RNN class
+            opt = torch.optim.SGD(rnn.parameters(), lr=t_dict['learning_rate'])  # call optimiser from pytorhc
+            rnn.set_info(param_dict={**d_dict, **t_dict})
+
+            ## Train with BPTT
+            rnn = bptt_training(rnn=rnn, optimiser=opt, dict_training_params=t_dict,
+                        x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test,
+                        verbose=0)
+
+            ## Decode cross temporally
+            score_mat, decoder_dict = train_decoder(rnn_model=rnn, x_train=x_train, x_test=x_test,
+                                               labels_train=labels_train, labels_test=labels_test,
+                                               dict_training_params=t_dict, save_inplace=True)
+
+            ## Save results:
+            rnn.save_model()
+        return rnn  # return latest
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt, exit')
